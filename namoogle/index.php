b8911c0f03c4488da4d3e6c3704a8f4c1cd80987
Nam Hoang Nguyen <35542721+Nam-H-Nguyen@users.noreply.github.com>
2019-11-07 17:10:28 -0800

    Reverse Engineering Google Search Engine, add index HTML, add search HTML

---
+++ namoogle/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>Welcome to Namoogle Search Engine!</title>
+        <link rel="stylesheet" type="text/css" href="assets/css/style.css"/>
+    </head>
+    <body>
+        <div class="mainContainer indexPage">
+
+            <div class="main">
+                <div class="logo">
+                    <img src="https://drive.google.com/uc?export=view&id=1cWtKYOL6eOg4_A15a9R3zRFGl2LSJa1A" alt="Namoogle">
+                </div>
+                <div class="search">
+                    <!-- Send the value to search.php with a GET method-->
+                    <form class="" action="search.php" method="GET">
+                        <input class="searchForm" type="text" name="query">
+                        <input class="searchButton" type="submit" value="Search">
+                    </form>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
