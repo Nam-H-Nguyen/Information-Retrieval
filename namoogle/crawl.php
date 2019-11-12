801be9d896630fe8f514d3d08b37e426ac2843c9
Nam Hoang Nguyen <35542721+Nam-H-Nguyen@users.noreply.github.com>
2019-11-12 14:51:55 -0800

    Parse all HTML a tags and create valid url links

--- namoogle/crawl.php
+++ namoogle/crawl.php
@@ -2,8 +2,36 @@
 include("./classes/DomDocumentParser.php");
 
 function createLink($src, $url) {
-    echo "SRC: $src<br>";
-    echo "URL $url<br>";
+    /*
+    Edge Cases for url links
+    scheme: http https
+    host: www.apple.com
+
+    #
+    javascript:
+
+    //www.apple.com
+    /contact/company.php
+    ./contact/company.php
+    ../contact/company.php
+    contact/company.php
+    */
+   $scheme = parse_url($url)["scheme"]; // http https
+   $host = parse_url($url)["host"]; // www.apple.com
+
+   if (substr($src, 0, 2) == "//") {
+       // scheme = http https
+       $src = $scheme . ":" . $src;
+   } else if (substr($src, 0, 1) == "/") {
+       $src = $scheme . "://" . $host . $src;
+   } else if (substr($src, 0, 2) == "./") {
+       $src = $scheme . "://" . $host . dirname(parse_url($url)["path"]) . substr($src, 1);
+   } else if (substr($src, 0, 3) == "../" ||
+              (substr($src, 0, 4) != "http" && substr($src, 0, 5) != "https")) {
+       $src = $scheme . "://" . $host . "/" . $src;
+   }
+
+   return $src;
 }
 
 function followLinks($url) {
@@ -21,8 +49,9 @@ function followLinks($url) {
             continue;
         }
 
-        createLink($href, $url);
-        // echo $href . "<br>";
+        $href = createLink($href, $url);
+
+        echo $href . "<br>";
     }
 }
 
