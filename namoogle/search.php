93004d902215232d08b08392172a09b69627fe49
Nam Hoang Nguyen <35542721+Nam-H-Nguyen@users.noreply.github.com>
2019-11-14 01:20:57 -0800

    Add pagination logic for selected page, pages left to be shown

--- namoogle/search.php
+++ namoogle/search.php
@@ -90,12 +90,68 @@
 			<!-- Printing out the number of search results -->
 			<?php
 				$resultsProvider = new SiteResultsProvider($con);
-				$pageLimit = 20;
+				$pageSize = 20;
 
 				$numResults = $resultsProvider->getNumResults($query);
 				echo "<p class='resultsCount'>$numResults results found</p>";
-				echo $resultsProvider->getResultsHTML($page, $pageLimit, $query);
+				echo $resultsProvider->getResultsHTML($page, $pageSize, $query);
 			?>
+		</div>
+
+		<div class="paginationContainer">
+			<div class="pageButtons">
+				<div class="pageNumberContainer">
+					<img src="https://drive.google.com/uc?export=view&id=1Lxu_8gNIDjEUjtGv0ZnCYBDxbej1rLdw">
+				</div>
+
+				<?php
+
+				// Show only 10 pages
+				$pagesToShow = 10;
+
+				// Number of search results divided by page size to get num pages needed
+				// to be shown from start to end
+				$numPages = ceil($numResults / $pageSize);
+
+				// If user clicks nearing towards the end, how many pages left to be shown
+				$pagesLeft = min($pagesToShow, $numPages);
+
+				// If click page 10, will show 5 pages to the left and 5 pages to the right
+				$currentPage = $page - floor($pagesToShow / 2);
+
+				// Case: current page selected, above and below current selection won't go
+				// below 1
+				if ($currentPage < 1) {
+					$currentPage = 1;
+				}
+				
+				while ($pagesLeft != 0) {
+					// Current page is not clickable
+					if ($currentPage == $page) {
+						echo "<div class='pageNumberContainer'>
+								<img src='https://drive.google.com/uc?export=view&id=1KSbYectB7FhIRijdv9OwI4oXlXNhn3TQ'>
+								<span class='pageNumber'>$currentPage</span>
+							  </div>";
+					} else {
+					// Surround numbers with clickable anchor tags to query search page number
+						echo "<div class='pageNumberContainer'>
+								<a href='search.php?query=$query&type=$type&page=$currentPage'>
+									<img src='https://drive.google.com/uc?export=view&id=1xlT7PGYX5L22chm8ZxCmSE0KF0BUdLxP'>
+									<span class='pageNumber'>$currentPage</span>
+								</a>
+							  </div>";
+
+					}
+					$currentPage++;
+					$pagesLeft--;
+				}
+
+				?>
+
+				<div class="pageButtons">
+					<img src="https://drive.google.com/uc?export=view&id=17v9VuKcGguCwJp4ovW0xgjfgzdgy4FHv">
+				</div>
+			</div>
 		</div>
 	</div>
 
